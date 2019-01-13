fac6783e7f468cb547f45520bbffc7e1b7abdec7
MolnarArthur <arthur.molnar@usource.ro>
2019-01-13 21:11:42 +0200

    Build routes options

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+router.get('/', (req, res) => {
+	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
+})
+
+/*  This route render json data */
+router.get('/json', (req, res) => {
+	res.json({
+		confirmation: 'success',
+		app: process.env.TURBO_APP_ID,
+		data: 'this is a sample json route.'
+	})
+})
+
+/*  This route sends text back as plain text. */
+router.get('/send', (req, res) => {
+	res.send('This is the Send Route')
+})
+
+/*  This route redirects requests to Turbo360. */
+router.get('/redirect', (req, res) => {
+	res.redirect('https://www.turbo360.co/landing')
+})
+
+
+module.exports = router
